0aa2b36d824b73a29239124ab45947bbe0c6010e
softwarejc <jcarlos.sanchez.calle@gmail.com>
2015-09-06 14:17:10 +0200

    Remove keys

--- DocumentDB.Framework.Tests/Helpers/UnitTestsConfig.cs
+++ DocumentDB.Framework.Tests/Helpers/UnitTestsConfig.cs
@@ -2,10 +2,10 @@
 {
     public static class UnitTestsConfig
     {
-        public static string EndPoint => "https://myfood.documents.azure.com:443";
+        public static string EndPoint => "";
 
-        public static string AuthorizationKey => "JWlMT0PzbP0cEgapdjcLsp2own295I8DK01tIHiDIxXxLXpnPkA7kIoq7pF+kcd2KlHqj/FuFg1oAc7B0lgbug==";
+        public static string AuthorizationKey => "";
 
-        public static string Database => "UnitTestsDatabase";
+        public static string Database => "";
     }
 }
